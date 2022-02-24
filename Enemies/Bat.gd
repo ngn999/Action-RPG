045a90d1461cb5dc260bb82d1b955abb6f2112f3
ngn999 <ngn998@gmail.com>
2022-02-24 10:41:01 +0800

    P15 reuse Effect.gd
    * how to use connect()
    * how to add node to same parent
    * how to use preload()

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
+const EnemyDeathEffect = preload("res://Enemies/EnemyDeathEffect.tscn")
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
@@ -14,3 +15,6 @@ func _on_Hurtbox_area_entered(area):
 
 func _on_Stats_no_health():
 	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
